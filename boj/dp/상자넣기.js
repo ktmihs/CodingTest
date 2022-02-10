678e1facedf81fab092603e508c62ae8d64cad12
ktmihs <yellowgreen423@gmail.com>
2022-02-10 15:45:23 +0900

    docs: solved dp problem - 상자넣기.js

---
+++ boj/dp/상자넣기.js
@@ -0,0 +1,20 @@
+// 1965
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = +input[0];
+const box = input[1].split(' ').map(Number);
+
+const dp = new Array(n).fill(0);
+dp[0] = 1;
+
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (box[i] > box[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+  }
+  dp[i]++;
+}
+
+console.log(Math.max(...dp));
