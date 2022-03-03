be8ea9a165355d6c62a61b351ecc318d51c2fedd
ktmihs <yellowgreen423@gmail.com>
2022-03-03 21:34:36 +0900

    docs: solved binary search problem - 꼬인 전깃줄.js

---
+++ boj/binary_search/꼬인 전깃줄.js
@@ -0,0 +1,27 @@
+// 1365
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const arr = input[1].split(' ').map(Number);
+const save = [0];
+
+const BS = n => {
+  let left = 0, right = save.length;
+  let answer = 0;
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (save[middle] < n) {
+      answer = middle;
+      left = middle + 1;
+    } else right = middle - 1;
+  }
+  return answer;
+}
+
+for (let i = 0; i < +input[0]; i++) {
+  const index = BS(arr[i]);
+  if (save.length <= index) save.push(arr[i]);
+  else save[index + 1] = arr[i];
+}
+console.log(+input[0] - save.length + 1);
