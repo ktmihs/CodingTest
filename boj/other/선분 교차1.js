63a70b3365d8ec356eb7d6d4b18b84928456f92c
ktmihs <yellowgreen423@gmail.com>
2022-03-05 18:40:22 +0900

    docs: solved geometry problem - 선분 교차1.js

---
+++ boj/other/선분 교차1.js
@@ -0,0 +1,18 @@
+// 17386
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const A = input[0].split(' ').map(Number);
+const B = input[1].split(' ').map(Number);
+
+const CCW = (x, y, w) => {
+  const result = (y[0] - x[0]) * (w[1] - x[1]) - (y[1] - x[1]) * (w[0] - x[0]);
+  if (!result) return 0;
+  return result > 0 ? 1 : -1;
+}
+
+const first = CCW([A[0], A[1]], [A[2], A[3]], [B[0], B[1]]) * CCW([A[0], A[1]], [A[2], A[3]], [B[2], B[3]]);
+const second = CCW([B[0], B[1]], [B[2], B[3]], [A[0], A[1]]) * CCW([B[0], B[1]], [B[2], B[3]], [A[2], A[3]]);
+if (first <= 0 && second <= 0) console.log(1);
+else console.log(0);
