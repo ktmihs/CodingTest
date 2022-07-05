44025de1d1fa5602e2568e3dcbb40cb8b5fd9bfb
ktmihs <yellowgreen423@gmail.com>
2022-07-05 16:49:22 +0900

    docs: solved string problem
    - 서로 다른 부분 문자열의 개수(11478).js

---
+++ boj/string/서로 다른 부분 문자열의 개수.js
@@ -0,0 +1,9 @@
+// 11478
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n")[0];
+
+const set = new Set();
+for (let i = 0; i <= input.length; i++) {
+  for (let j = i + 1; j <= input.length; j++) set.add(input.slice(i, j));
+}
+console.log(set.size);
