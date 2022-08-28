9adf0a27887b09b1d68db3d0a0c6b7687a0f8ad6
ktmihs <yellowgreen423@gmail.com>
2022-08-28 22:42:20 +0900

    docs: solved BFS problem - 불(5427).js

---
+++ boj/DFS_BFS/불.js
@@ -0,0 +1,54 @@
+// 5427
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n").slice(1);
+
+const result = [];
+while (input.length) {
+  const [m, n] = input.shift().split(' ').map(Number);
+  const visit = [...new Array(n)].map(e => Array(m).fill(0));
+  const maze = input.slice(0, n).map(line => line.split(''));
+  input = input.slice(n);
+  const DIR = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+
+  const findPos = word => {
+    const res = [];
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) if (maze[i][j] === word) res.push([i, j]);
+    }
+    return res;
+  }
+
+  const BFS = () => {
+    let queue = findPos('@'), fire = findPos('*'), answer = 1;
+    while (queue.length) {
+      const tmp = queue.slice();
+      const fireTmp = fire.slice();
+      queue = [];
+      fire = [];
+
+      for (const [x, y] of fireTmp) {
+        for (const [dx, dy] of DIR) {
+          if (x + dx >= 0 && x + dx < n && y + dy >= 0 && y + dy < m && !visit[x + dx][y + dy] && maze[x + dx][y + dy] !== '#') {
+            visit[x + dx][y + dy] = 1;
+            fire.push([x + dx, y + dy]);
+          }
+        }
+      }
+
+      for (const [x, y] of tmp) {
+        if (!x || !y || x === n - 1 || y === m - 1) return answer;
+        for (const [dx, dy] of DIR) {
+          if (x + dx >= 0 && x + dx < n && y + dy >= 0 && y + dy < m && !visit[x + dx][y + dy] && maze[x + dx][y + dy] === '.') {
+            visit[x + dx][y + dy] = 1;
+            queue.push([x + dx, y + dy]);
+          }
+        }
+      }
+      answer++;
+    }
+    return 'IMPOSSIBLE';
+  }
+  result.push(BFS());
+}
+
+console.log(result.join('\n'));
