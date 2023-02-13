49ab7ef5187716b7df6e54a417016969e6d7fe8f
ktmihs <yellowgreen423@gmail.com>
2023-02-13 18:10:49 +0900

    [docs] Solved programmers problem
    - 둘만의 암호

--- programmers/연습문제/[Lv1] 둘만의 암호.js
+++ programmers/연습문제/[Lv1] 둘만의 암호.js
@@ -1,4 +1,13 @@
 function solution(s, skip, index) {
-  var answer = '';
-  return answer;
+  const list = [];
+
+  for (let i = 0; i < 26; i++) {
+    const char = String.fromCharCode(97 + i);
+    if (skip.indexOf(char) === -1) list.push(char);
+  }
+
+  return [...s].map(char => {
+    const idx = list.indexOf(char);
+    return list[(idx + index) % list.length];
+  }).join('');
 }
