923a42ebf5ac836a25464a7425bd0cd8c9ba8ea1
ktmihs <63227474+ktmihs@users.noreply.github.com>
2023-03-17 17:26:41 +0900

    Delete [Lv1] 크기가 작은 부분 문자열.js

--- programmers/연습문제/[Lv1] 크기가 작은 부분 문자열.js
+++
@@ -1,9 +0,0 @@
-function solution(t, p) {
-  let answer = 0;
-  const T_LEN = t.length, P_LEN = p.length;
-
-  for (let i = 0; i <= T_LEN - P_LEN; i++) {
-    if (+t.slice(i, i + P_LEN) <= +p) answer++;
-  }
-  return answer;
-}
