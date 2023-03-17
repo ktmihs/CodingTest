09238d24930830719cfef1742613ea942bf68386
ktmihs <63227474+ktmihs@users.noreply.github.com>
2023-03-17 17:22:41 +0900

    Delete programmers/연습문제/spring intern directory

--- programmers/연습문제/spring intern/test3.js
+++
@@ -1,9 +0,0 @@
-function solution(queries) {
-  return queries.map(query => {
-    const len = query.length;
-    const front = query.slice(0, Math.floor(len / 2));
-    const back = query.slice(Math.ceil(len / 2)).reverse();
-
-    return front.reduce((total, num, idx) => total + Math.abs(num - back[idx]), 0) % 2;
-  })
-}
