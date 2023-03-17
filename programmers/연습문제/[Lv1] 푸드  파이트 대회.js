7a58b5cf9d93240e79a4db6cc73022e8a844c8e0
ktmihs <63227474+ktmihs@users.noreply.github.com>
2023-03-17 17:25:54 +0900

    Delete [Lv1] 푸드  파이트 대회.js

--- programmers/연습문제/[Lv1] 푸드  파이트 대회.js
+++
@@ -1,5 +0,0 @@
-function solution(food) {
-  let answer = '';
-  food.forEach((cal, idx) => answer += `${idx}`.repeat(Math.floor(cal / 2)))
-  return answer + '0' + answer.split('').reverse().join('');
-}
